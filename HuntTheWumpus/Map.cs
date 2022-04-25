f9e703594dff3ebe5b896d5c687406af4b7a731c
Maxim Pomogaev <coldfrontlich@gmail.com>
2022-04-25 16:05:51 +0300

    Create map

---
+++ HuntTheWumpus/Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntTheWumpus
+{
+    public class Map
+    {
+        public Map(byte mapSize)
+        {
+
+        }
+
+    }
+}
