37532222b12e9df236ab17c0ffd0b507ddce6beb
Maxim Pomogaev <coldfrontlich@gmail.com>
2022-04-25 14:51:27 +0300

    make GameObjects

---
+++ HuntTheWumpus/GameObjects/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntTheWumpus.GameObjects
+{
+    public class Player
+    {
+
+    }
+
+    public class Wumpus
+    {
+
+    }
+
+    public class Bat
+    {
+
+    }
+
+    public class Pit
+    {
+
+    }
+}
